bb5f17d6c397cbd147af21265224061491b5be64
gitGyu <52170581+hangyujung@users.noreply.github.com>
2021-02-07 00:52:30 +0900

    Create 타겟 넘버.java

---
+++ 타겟 넘버.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
+    }
+    int dfs(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
+    }
+}
+
+//DFS/BFS LV2
