2256a8dbfdd97ad1ccbd5630a7c052ee8ea4108d
gitGyu <52170581+hangyujung@users.noreply.github.com>
2021-04-07 20:40:19 +0900

    Update 스킬트리.java

--- 스킬트리.java
+++ 스킬트리.java
@@ -1,25 +1,23 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        		int answer = 0;
-		
-		int index = 0;
-		while(true) {
-			String skill_tree = skill_trees[index];
-			String skill_clone = new String(skill_tree);
-			int size = skill_tree.length();
-			for (int i =0; i < size; i++) {
-				String oneSkill = String.valueOf(skill_clone.charAt(i));
-				if (!skill.contains(oneSkill)) {
-					skill_tree = skill_tree.replace(oneSkill, "");
-				}
-			}
-			if (skill.indexOf(skill_tree) == 0) {
-				answer++;
-			}
-			index++;
-			if(index == skill_trees.length) break;
-		}
-	
-		return answer;
+        int answer = 0;
+        for (String skillTree : skill_trees) {
+            int learningIdx = 0;
+            boolean isAble = true;
+            for (char curSkill : skillTree.toCharArray()) {
+                int skillIdx = skill.indexOf(curSkill);
+                if (skillIdx == -1)
+                    continue;
+                else if (skillIdx == learningIdx)
+                    learningIdx++;
+                else {
+                    isAble = false;
+                    break;
+                }
+            }
+            if (isAble)
+                answer++;
+        }
+        return answer;
     }
 }
