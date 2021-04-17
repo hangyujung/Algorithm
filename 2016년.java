79199c37cf6046fccd7589288d29ce3a0f0cea07
gitGyu <52170581+hangyujung@users.noreply.github.com>
2021-04-17 17:24:25 +0900

    Create 2016년.java

---
+++ 2016년.java
@@ -0,0 +1,23 @@
+class TryHelloWorld
+{
+    public String getDayName(int a, int b) {
+        String answer = "";
+        String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int allDate = 0;
+        for (int i = 0; i < a - 1; i++) {
+            allDate += date[i];
+        }
+        allDate += (b - 1);
+        answer = day[allDate % 7];
+
+        return answer;
+    }
+    public static void main(String[] args)
+    {
+        TryHelloWorld test = new TryHelloWorld();
+        int a=5, b=24;
+        System.out.println(test.getDayName(a,b));
+    }
+}
+
