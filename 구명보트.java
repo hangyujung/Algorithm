0e81711697964cca585ba809f7be7b53a997c448
gitGyu <52170581+hangyujung@users.noreply.github.com>
2021-04-20 15:19:09 +0900

    Create 구명보트.java

---
+++ 구명보트.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
+        }
+        return people.length - i;
+    }
+}
