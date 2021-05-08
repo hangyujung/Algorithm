3d93bba114b5b1bddb4b9df9aac6c39d4d214850
gitGyu <52170581+hangyujung@users.noreply.github.com>
2021-05-08 23:28:54 +0900

    Create 최댓값과 최솟값.java

---
+++ 최댓값과 최솟값.java
@@ -0,0 +1,16 @@
+class Solution {
+  public String solution(String str) {
+      
+        String[] tmp = str.split(" ");
+        int min, max, n;
+        min = max = Integer.parseInt(tmp[0]);
+        for (int i = 1; i < tmp.length; i++) {
+            n = Integer.parseInt(tmp[i]);
+            if(min > n) min = n;
+            if(max < n) max = n;
+        }
+ 
+        return min + " " + max;
+ 
+  }
+}
