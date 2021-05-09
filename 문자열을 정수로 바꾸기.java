973b6815a833007c0f05cf40af576e601d70336b
gitGyu <52170581+hangyujung@users.noreply.github.com>
2021-05-09 22:45:56 +0900

    Create 문자열을 정수로 바꾸기.java

---
+++ 문자열을 정수로 바꾸기.java
@@ -0,0 +1,10 @@
+public class StrToInt {
+    public int getStrToInt(String str) {
+                return Integer.parseInt(str);
+    }
+    //아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String args[]) {
+        StrToInt strToInt = new StrToInt();
+        System.out.println(strToInt.getStrToInt("-1234"));
+    }
+}
