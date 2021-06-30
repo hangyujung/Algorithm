fe4a7a203112cf4aae8fcd9fa9c7c445b4cb40ab
gitGyu <52170581+hangyujung@users.noreply.github.com>
2021-06-30 15:47:39 +0900

    Update 피보나치 함수.java

--- BOJ/DP/피보나치 함수.java
+++ BOJ/DP/피보나치 함수.java
@@ -1,40 +1,31 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
- 
-	static Integer[][] dp = new Integer[41][2];
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		
-		dp[0][0] = 1;	// N=0 일 때의 0 호출 횟수
-		dp[0][1] = 0;	// N=0 일 때의 1 호출 횟수
-		dp[1][0] = 0;	// N=1 일 때의 0 호출 횟수
-		dp[1][1] = 1;	// N=1 일 때의 1 호출 횟수
+		int[] arr = new int[T];
 		
-		int T = in.nextInt();
-        
-		StringBuilder sb = new StringBuilder();
- 
-		while(T-- > 0){
-			int N = in.nextInt();
-			fibonacci(N);
-			sb.append(dp[N][0] + " " + dp[N][1]).append('\n');
+		for(int i=0;i<T;i++) arr[i] = Integer.parseInt(br.readLine());
+		
+		int[] zero = new int[41];
+		int[] one = new int[41];
+		
+		
+		zero[0] = 1;
+		zero[1] = 0;
+		one[0] = 0;
+		one[1] = 1;
+		
+		for(int i=2;i<41;i++) {
+			zero[i] = zero[i-1] + zero[i-2];
+			one[i] = one[i-1] + one[i-2];
 		}
-		System.out.print(sb);
+		
+		for(int i=0;i<T;i++) System.out.printf("%d %d\n",zero[arr[i]],one[arr[i]]);
 	}
-	
-	static Integer[] fibonacci(int N) {
-		// N에 대한 0, 1의 호출 횟수가 없을 떄(탐색하지 않은 값일 때)
-		if(dp[N][0] == null || dp[N][1] == null) {
-			// 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
-			dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
-			dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
-		}
-		// N에 대한 0과 1, 즉 [N][0]과 [N][1] 을 담고있는 [N]을 반환한다.
-		return dp[N];
- 
-	}
- 
 }
- 
