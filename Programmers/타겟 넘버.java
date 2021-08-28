89f5bc34f546f9f5065724d2b74d33e74963108f
gitGyu <ssikssikii5@gmail.com>
2021-08-29 00:58:20 +0900

    Update 타겟 넘버.java

--- Programmers/타겟 넘버.java
+++ Programmers/타겟 넘버.java
@@ -1,19 +1,61 @@
 
+// class Solution {
+//     public int solution(int[] numbers, int target) {
+//         int answer = 0;
+//         answer = dfs(numbers, 0, 0, target);
+//         return answer;
+//     }
+//     int dfs(int[] numbers, int n, int sum, int target) {
+//         if(n == numbers.length) {
+//             if(sum == target) {
+//                 return 1;
+//             }
+//             return 0;
+//         }
+//         return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
+//     }
+// }
+
+
+
 class Solution {
+    static boolean visited[];
+    static int total,count,N;
+    //static int[] numbers;
+    
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        answer = dfs(numbers, 0, 0, target);
-        return answer;
-    }
-    int dfs(int[] numbers, int n, int sum, int target) {
-        if(n == numbers.length) {
-            if(sum == target) {
-                return 1;
-            }
-            return 0;
+        N = numbers.length;
+        visited=new boolean[N];
+       // numbers=numbers;
+        for(int data:numbers){
+            total+=data;
         }
-        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
+        powerSet(target, 0, numbers);
+        return count;
+    }
+    
+    static void powerSet(int target, int num,int[] numbers){
+        if(num==N){
+            int sum=0;
+       
+            for(int i=0;i<visited.length;i++){
+                if(visited[i]) sum+= numbers[i];
+                else { sum -= numbers[i];}
+            }
+            if(sum==target) {
+                count++;
+                System.out.println(sum);
+            } 
+            return;
+        }
+        
+      
+            visited[num] = true;
+            powerSet(target,num+1,numbers);
+            visited[num]= false;
+            powerSet(target,num+1,numbers);
+        
     }
 }
-
 //DFS/BFS LV2
